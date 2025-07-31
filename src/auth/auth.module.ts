e1844759303a70cc970d4c3c30e89b4c93136836
smtamnay <smtamay@ixmatix.com>
2025-07-31 11:01:30 -0600

    ✨ feat: implement authentication module with JWT strategy, login functionality, and DTOs

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '1d' },
+    }),
+    forwardRef(() => UsersModule),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
